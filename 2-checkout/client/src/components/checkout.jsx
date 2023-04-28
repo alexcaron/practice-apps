2fa90f7ca23d5dcd8686e3c53b9028953aacd2c0
Alex Caron <alex.caron@gmail.com>
2023-04-28 18:56:29 -0500

    save shipping info form to the form state in client

--- 2-checkout/client/src/components/checkout.jsx
+++ 2-checkout/client/src/components/checkout.jsx
@@ -6,11 +6,14 @@ const Checkout = () => {
   const [currentForm, setCurrentForm] = useState(1);
   const [formInfo, setFormInfo] = useState({});
   const createAccount = (info) => {
-    // create account helper function
     setFormInfo(info);
-    console.log(info);
-    // move to form 2
     setCurrentForm(2);
+  };
+
+  const addShipping = (info) => {
+    const newFormInfo = Object.assign(formInfo, info);
+    setFormInfo(newFormInfo);
+    setCurrentForm(3);
   };
 
   return (
@@ -25,7 +28,7 @@ const Checkout = () => {
       <h3>Step 1: Create an account</h3>
       {currentForm === 1 && <Account create={ createAccount }/>}
       <h3>Step 2: Provide your shipping details</h3>
-      {currentForm === 2 && <Shipping />}
+      {currentForm === 2 && <Shipping addShipping={ addShipping }/>}
     </div></>
   );
 };
